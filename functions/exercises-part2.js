48d428cf4df3e063663639bfbfb6c4026f1cfefa
Clifford Fajardo <fajardoclifford@gmail.com>
2017-11-04 17:13:18 -0700

    finish functions exercises

---
+++ functions/exercises-part2.js
@@ -0,0 +1,63 @@
+//*************************START: Exercise 1************************************
+
+//TODO: create a function called printName which should take a name as argument and console.log the name passed in.
+
+
+//TODO: create an array called 'nameList' which should be an array of strings.
+
+
+
+//TODO: create a function called `logAllNames`, which takes in a function and a list of names
+// For example: logAllNames(printName, ['anne', 'bob']) should log
+      //'anne'
+      //'bob'
+
+
+
+
+
+//*************************END: Exercise 1************************************
+
+
+
+
+
+
+
+
+
+
+
+//*************************START: Exercise 2************************************
+//TODO: 1. create a function called 'doubler' which will take
+
+
+//TODO: 2.create an array called `integerList` and fill it with a list of numbers. We'll be using this later
+
+
+//TODO 3: create a function called `doubleIntegerList` which will take in function `func` and a list called `numList`.
+//        When we call `doubleIntegerList` like so: doubleIntegerList(doubler, [1,2,3])
+//        `doubleIntegerList` should the list that was passed in but doubled; we're not returning an entirely new array.
+//*************************END: Exercise 2************************************
+
+
+
+
+
+
+
+
+
+//*************************START: Exercise 3************************************
+
+
+//TODO: create a function called `map` which takes in a `list` and a function.
+//      `map` applies the function it gets passed and applies that function on each item in the list.
+//      Example:
+//      let results = map([1,2,3], function add1(num){
+//                      return num +1
+//                    })
+//     console.log(results) --> [2,3,4]
+
+
+//*************************END: Exercise 3************************************
--- functions/promise1.js
+++
@@ -1,8 +0,0 @@
-function promise1(file) {
-	return new Promise(function(resolve, reject){
-		// fakeAjax(file, function(data){
-		// 	resolve(data)
-		// });
-		resolve('promise resolved')
-	});
-}
